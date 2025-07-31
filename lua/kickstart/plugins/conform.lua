a6202276f1ae273f67ac23c8866f2a61b3f4da34
Omar Myers <Omar.Myers@torchtechnologies.com>
2025-07-31 13:32:30 -0500

    feat: update conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -61,6 +61,7 @@ return {
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'prettierd', 'prettier', stop_after_first = true },
         jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
