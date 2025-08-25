3c2dfd9b65869665e017f1b49a924671f3748661
Omar Myers <Omar.Myers@torchtechnologies.com>
2025-08-25 11:38:50 -0500

    fix: remove deprecated nvim-ts-autotag usage in treesitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -32,11 +32,6 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
-
-      -- Enable nvim-ts-autotag
-      autotag = {
-        enable = true,
-      },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
